9b7372010b001c52e76cb3b0e3f9d865fb280709
Minhduc027 <minhduylongthuan@gmail>
2024-05-22 10:46:17 +0700

    Update Post and Category class

--- GoodsExchange.business/Interface/ICategoryBusiness.cs
+++ GoodsExchange.business/Interface/ICategoryBusiness.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GoodsExchange.data.Models;
 
 namespace GoodsExchange.business.Interface
 {
     internal interface ICategoryBusiness
     {
         public Task<IGoodsExchangeResult> GetAllCategory();
-        public Task<IGoodsExchangeResult> CreateCategory();
-        public Task<IGoodsExchangeResult> UpdateCategory();
-        public Task<IGoodsExchangeResult> DeleteCategory();
+        public Task<IGoodsExchangeResult> CreateCategory(Category category);
+        public Task<IGoodsExchangeResult> UpdateCategory(Category category);
+        public Task<IGoodsExchangeResult> DeleteCategory(int categoryId);
     }
 }
--- GoodsExchange.business/PostBusiness.cs
+++ GoodsExchange.business/PostBusiness.cs
@@ -1,5 +1,6 @@
 ﻿using GoodsExchange.business.Interface;
 using GoodsExchange.data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,30 +12,84 @@ namespace GoodsExchange.business
 
     public class PostBusiness : IPostBusiness
     {
+        private static string CREATE_SUCCESS = "Create successfully!";
+        private static string ERROR_EXECUTING_TASK = "Error while executing task: ";
+        private static string NOT_FOUND = "Post not found";
+        private static string DELETED = "Post deleted!";
+        private static string SUCCESS = "Task executed successfully";
         private readonly Net1710_221_7_GoodsExchangeContext _context;
         public PostBusiness(Net1710_221_7_GoodsExchangeContext context)
         {
             _context = context;
         }
 
-        public Task<IGoodsExchangeResult> Create()
+        public async Task<IGoodsExchangeResult> Create(Post postCreate)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _context.AddAsync(postCreate);
+                await _context.SaveChangesAsync();
+
+                return new GoodsExchangeResult(0, CREATE_SUCCESS, postCreate);
+            } catch (Exception ex)
+            {
+                return new GoodsExchangeResult(-1, ERROR_EXECUTING_TASK + ex.Message);
+            }
         }
 
-        public Task<IGoodsExchangeResult> Delete()
+        public async Task<IGoodsExchangeResult> Delete(int postId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var post = await _context.Posts.FindAsync(postId);
+                if (post == null)
+                {
+                    return new GoodsExchangeResult(-1, NOT_FOUND);
+                }
+                _context.Posts.Remove(post);
+                await _context.SaveChangesAsync();
+
+                return new GoodsExchangeResult(0, DELETED, post);
+            } catch(Exception ex)
+            {
+                return new GoodsExchangeResult(-1, ERROR_EXECUTING_TASK + ex.Message);
+            }
         }
 
-        public Task<IGoodsExchangeResult> GetAll()
+        public async Task<IGoodsExchangeResult> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var postList = await _context.Posts.Include(c => c.Comments).Include(o => o.OfferDetails)
+                    .ToListAsync();
+                return new GoodsExchangeResult(0, SUCCESS, postList);
+            } catch(Exception ex)
+            {
+                return new GoodsExchangeResult(-1, ERROR_EXECUTING_TASK + ex.Message);
+            }
         }
 
-        public Task<IGoodsExchangeResult> Update()
+        public async Task<IGoodsExchangeResult> Update(Post postUpdate)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var post =  await _context.Posts.FindAsync(postUpdate.PostId);
+                if(post == null)
+                {
+                    return new GoodsExchangeResult(-1, NOT_FOUND);
+                }
+                post.Title = postUpdate.Title;
+                post.Description = postUpdate.Description;
+                post.Address = postUpdate.Address;
+                post.Category = postUpdate.Category;
+                post.CategoryId = postUpdate.Category.CategoryId;
+                await _context.SaveChangesAsync();
+
+                return new GoodsExchangeResult(0, SUCCESS, post);
+            } catch(Exception ex)
+            {
+                return new GoodsExchangeResult(-1, ERROR_EXECUTING_TASK + ex.Message);
+            }
         }
     }
 }
