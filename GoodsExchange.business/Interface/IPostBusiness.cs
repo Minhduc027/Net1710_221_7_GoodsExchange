4b6d7163c834a45d9fbbbeeb9788b82af0f31102
Minhduc027 <minhduylongthuan@gmail>
2024-05-22 12:15:18 +0700

    Update PostBusiness

--- GoodsExchange.business/Interface/IPostBusiness.cs
+++ GoodsExchange.business/Interface/IPostBusiness.cs
@@ -10,6 +10,9 @@ namespace GoodsExchange.business.Interface
     public interface IPostBusiness
     {
         public Task<IGoodsExchangeResult> GetAll();
+        public Task<IGoodsExchangeResult> GetById(int id);
+        public Task<IGoodsExchangeResult> GetByCreateDate(DateTime createDate);
+        public Task<IGoodsExchangeResult> GetByUser(int userId);
         public Task<IGoodsExchangeResult> Create(Post post);
         public Task<IGoodsExchangeResult> Update(Post post);
         public Task<IGoodsExchangeResult> Delete(int postId);
--- GoodsExchange.business/PostBusiness.cs
+++ GoodsExchange.business/PostBusiness.cs
@@ -16,7 +16,7 @@ namespace GoodsExchange.business
         private static string ERROR_EXECUTING_TASK = "Error while executing task: ";
         private static string NOT_FOUND = "Post not found";
         private static string DELETED = "Post deleted!";
-        private static string SUCCESS = "Task executed successfully";
+        private static string SUCCESS = "Task executed successfully: ";
         private readonly Net1710_221_7_GoodsExchangeContext _context;
         public PostBusiness(Net1710_221_7_GoodsExchangeContext context)
         {
@@ -46,6 +46,14 @@ namespace GoodsExchange.business
                 {
                     return new GoodsExchangeResult(-1, NOT_FOUND);
                 }
+                //var comments = _context.Comments.Where(c => c.PostId == post.PostId).ToList();
+                //_context.Comments.RemoveRange(comments);
+                //await _context.SaveChangesAsync();
+
+                //var offerDetails = _context.OfferDetails.Where(c => c.PostId == postId).ToList();
+                //_context.OfferDetails.RemoveRange(offerDetails);
+                //await _context.SaveChangesAsync();
+
                 _context.Posts.Remove(post);
                 await _context.SaveChangesAsync();
 
@@ -62,7 +70,52 @@ namespace GoodsExchange.business
             {
                 var postList = await _context.Posts.Include(c => c.Comments).Include(o => o.OfferDetails)
                     .ToListAsync();
-                return new GoodsExchangeResult(0, SUCCESS, postList);
+                return new GoodsExchangeResult(0, SUCCESS + "Get all Post.", postList);
+            } catch(Exception ex)
+            {
+                return new GoodsExchangeResult(-1, ERROR_EXECUTING_TASK + ex.Message);
+            }
+        }
+
+        public async Task<IGoodsExchangeResult> GetByCreateDate(DateTime createDate)
+        {
+            try
+            {
+                var result = await _context.Posts.Where(p => p.CreateDate == createDate).ToListAsync();
+
+                return new GoodsExchangeResult(0, SUCCESS + "Get posts by create date.", result);
+            } catch (Exception ex)
+            {
+                return new GoodsExchangeResult(-1, ERROR_EXECUTING_TASK + ex.Message);
+            }
+        }
+
+        public async Task<IGoodsExchangeResult> GetById(int id)
+        {
+            try
+            {
+                var result = await _context.Posts.FindAsync(id);
+                if(result == null)
+                {
+                    return new GoodsExchangeResult(-1, NOT_FOUND);
+                }
+                return new GoodsExchangeResult(0, SUCCESS + "Get post by Id.", result);
+            } catch(Exception ex)
+            {
+                return new GoodsExchangeResult(-1, ERROR_EXECUTING_TASK + ex.Message);
+            }
+        }
+
+        public async Task<IGoodsExchangeResult> GetByUser(int userId)
+        {
+            try
+            {
+                var result = await _context.Posts.Include(p => p.PostOwnerId == userId).ToListAsync();
+                if(result == null)
+                {
+                    return new GoodsExchangeResult(0, SUCCESS + "Get User's post", "This user haven't create any post yet!");
+                }
+                return new GoodsExchangeResult(0, SUCCESS + $"Get User {userId} post!", result);
             } catch(Exception ex)
             {
                 return new GoodsExchangeResult(-1, ERROR_EXECUTING_TASK + ex.Message);
@@ -85,7 +138,7 @@ namespace GoodsExchange.business
                 post.CategoryId = postUpdate.Category.CategoryId;
                 await _context.SaveChangesAsync();
 
-                return new GoodsExchangeResult(0, SUCCESS, post);
+                return new GoodsExchangeResult(0, SUCCESS + "Post updated!", post);
             } catch(Exception ex)
             {
                 return new GoodsExchangeResult(-1, ERROR_EXECUTING_TASK + ex.Message);
