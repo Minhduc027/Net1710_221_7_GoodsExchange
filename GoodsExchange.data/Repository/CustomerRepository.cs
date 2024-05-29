a50b21f0898a013019232a2e05bce1aecba207ea
TinNTGaming <84866171+TinNTGaming@users.noreply.github.com>
2024-05-29 13:03:34 +0700

    Customer

---
+++ GoodsExchange.data/Repository/CustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using GoodsExchange.data.Base;
+using GoodsExchange.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodsExchange.data.Repository
+{
+    public class CustomerRepository : GenericRepository<Customer>
+    {
+        public CustomerRepository() { }
+    }
+}
--- GoodsExchange.data/UnitOfWork.cs
+++ GoodsExchange.data/UnitOfWork.cs
@@ -7,10 +7,16 @@ namespace GoodsExchange.data
     {
         private Net1710_221_7_GoodsExchangeContext _context;
         private PostRepository _postRepository;
+        private CustomerRepository _customerRepository;
         public UnitOfWork() { }
         public PostRepository PostRepository
         {
             get { return _postRepository ??= new PostRepository(); }
+        }
+
+        public CustomerRepository CustomerRepository
+        {
+            get { return _customerRepository ??= new CustomerRepository(); }
         }
     }
 }
