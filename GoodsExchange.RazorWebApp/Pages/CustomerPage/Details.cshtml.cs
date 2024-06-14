68bb318c24aaf97699d9ace77d834a7269e35077
TinNTGaming <84866171+TinNTGaming@users.noreply.github.com>
2024-06-15 03:12:20 +0700

    Customer CRUD

---
+++ GoodsExchange.RazorWebApp/Pages/CustomerPage/Details.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GoodsExchange.data.Models;
+using GoodsExchange.business.Interface;
+using GoodsExchange.business;
+
+namespace GoodsExchange.RazorWebApp.Pages.CustomerPage
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ICustomerBusiness _customerBusiness;
+
+        public DetailsModel()
+        {
+            _customerBusiness ??= new CustomerBusiness();
+        }
+
+        public Customer Customer { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _customerBusiness.GetCustomerById((int)id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Customer = customer.Data as Customer;
+            }
+            return Page();
+        }
+    }
+}
